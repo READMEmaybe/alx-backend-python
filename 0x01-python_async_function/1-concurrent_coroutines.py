a5cc05cdca3712edefc36937e0682a025fb83dab
READMEmaybe <haroune11411@gmail.com>
2024-04-22 16:57:33 +0100

    Fix type annotation

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python3
 """ Concurrent coroutines """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     Waits for a random delay between 0 and max_delay (inclusive)
     n times, and returns a list of the resulting delay times.
