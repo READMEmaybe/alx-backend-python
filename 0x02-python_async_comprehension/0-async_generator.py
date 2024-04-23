db701837e40f9bc38ff07fcb9870fe4d3cd5173d
READMEmaybe <haroune11411@gmail.com>
2024-04-23 20:10:05 +0100

    Add task 0

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,22 @@
+""" async_generator module."""
+import asyncio
+import random
+from typing import AsyncGenerator
+
+
+async def async_generator() -> AsyncGenerator[float, None]:
+    """
+    Asynchronous generator that yields random float numbers.
+
+    Yields:
+        float: A random float number.
+
+    Raises:
+        None
+
+    Returns:
+        None
+    """
+    for _ in range(10):
+        await asyncio.sleep(1)
+        yield random.random()
