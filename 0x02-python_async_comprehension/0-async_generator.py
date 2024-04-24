901405c099a00126fd8f11233304a63aa652aa6d
READMEmaybe <haroune11411@gmail.com>
2024-04-24 17:16:32 +0100

    Fix task 0

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """ async_generator module."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """
     Asynchronous generator that yields random float numbers.
 
